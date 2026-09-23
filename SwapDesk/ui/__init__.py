42c62ce54abb7c520750f97e7eaaf29fb0c7248d
swapdesk <swapdesk@proton.me>
2026-09-22 20:35:19 -0700

    Delete SwapDesk directory

--- SwapDesk/ui/__init__.py
+++
@@ -1,9 +0,0 @@
-"""SwapDesk UI tab mixins, split out of the app.py monolith."""
-from .swap_tab import SwapTabMixin
-from .deposit import DepositMixin
-from .settings_tab import SettingsTabMixin
-from .history_tab import HistoryTabMixin
-
-__all__ = [  # noqa: RUF022  (tab order, not alphabetical)
-    "SwapTabMixin", "DepositMixin", "SettingsTabMixin", "HistoryTabMixin",
-]
